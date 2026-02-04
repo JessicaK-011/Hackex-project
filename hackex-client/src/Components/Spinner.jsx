e0d032b1f2b5694ad7aa1da65424da7d9f4e290b
JessicaK-011 <jessicakamal054@gmail.com>
2026-02-05 01:37:23 +0530

    change global loader

--- hackex-client/src/Components/Spinner.jsx
+++ hackex-client/src/Components/Spinner.jsx
@@ -1,9 +1,21 @@
+// const Spinner = () => {
+//   return (
+//     <div className="fixed inset-0 flex items-center justify-center bg-transparent backdrop-blur-sm z-50">
+//       <div className="loader"></div>
+//     </div>
+//   );
+// };
+
+// export default Spinner;
 const Spinner = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-transparent backdrop-blur-sm z-50">
-      <div className="loader"></div>
+    <div className="fixed inset-0 z-50 bg-white/70 dark:bg-black/40 backdrop-blur-sm">
+      <div className="loader">
+        <div className="jimu-primary-loading"></div>
+      </div>
     </div>
   );
 };
 
 export default Spinner;
+
