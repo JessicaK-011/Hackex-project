732483cdea5ff1b71f470085f968b3fa70f81146
Jessica Kamal <jessicakamal054@gmail.com>
2026-08-07 01:59:38 +0530

    Update problemRoutes.js

--- hackex-server/routes/problemRoutes.js
+++ hackex-server/routes/problemRoutes.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const problemController = require("../controllers/problemController");
-
-router.get("/hello", problemController.hello);
+// Fetches every problem.
 router.get("/allProblems", problemController.getAllProblems);
+// Creates a new problem using data sent in req.body.
 router.post("/addProblem", problemController.addProblem);
+// Uses a route parameter (:id). Whatever value is placed in the URL (like /getProblem/65a123) is extracted inside the controller as req.params.id.
 router.get("/getProblem/:id", problemController.getProblem);
 module.exports = router;
