2fda6b363d8da786742e510c658aaf33e878549f
Jessica Kamal <jessicakamal054@gmail.com>
2026-08-10 10:25:41 +0530

    Update userRoutes.js

--- hackex-server/routes/userRoutes.js
+++ hackex-server/routes/userRoutes.js
@@ -10,7 +10,6 @@ const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
 
 router.get("/", userController.getAllUsers);
-router.get("/hello", userController.hello);
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
