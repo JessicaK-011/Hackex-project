279dae187ac08e100a6f16e45b0ba717df572fe0
Jessica Kamal <jessicakamal054@gmail.com>
2026-09-06 00:43:28 +0530

    Fix rate limit window duration to 1 hour

--- hackex-server/app.js
+++ hackex-server/app.js
@@ -33,7 +33,7 @@ app.use(helmet({ contentSecurityPolicy: false }));
 
 const limiter = rateLimit({
   max: 10000,
-  windowMs: 60 * 60 * 10000,
+  windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP. Please try again later.",
 });
 
